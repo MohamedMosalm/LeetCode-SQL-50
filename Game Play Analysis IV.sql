67a003e4c7f3d925584b2ebbe6c735f25a5b4dd9
Mohamed Mosalm <99332830+MohamedMosalm@users.noreply.github.com>
2024-05-31 09:33:27 +0300

    added Game Play Analysis IV

---
+++ Game Play Analysis IV.sql
@@ -0,0 +1,7 @@
+select round(count(player_id) / (select count(distinct player_id) from Activity) , 2) as fraction
+from Activity
+where (player_id,event_date) in (
+    select player_id, date_add(min(event_date),interval 1 day) as event_date
+    from Activity
+    group by player_id
+)
